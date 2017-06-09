0559d70df470011305608cdef46f54b2af3c7a85
Tom Wismar <tomjwismar@gmail.com>
2017-06-08 19:18:09 -0500

    complete

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,6 +23,7 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
    @RequestMapping (value = "results")
     public String search(Model model,@RequestParam String searchType,@RequestParam String searchTerm) {
 
